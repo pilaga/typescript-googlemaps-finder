7ee6c4b4ee6113dfad22a9cf5a0951e3a48910e0
Pierre <pierre.lagadec@gmail.com>
2021-10-28 11:08:17 +1100

    getting user input

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
 //
-console.log('Hello world!');
+const form = document.querySelector('form')! as HTMLFormElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function submitHandler(event: Event) {
+    event.preventDefault();
+
+    const enteredAddress = addressInput.value;
+    console.log('input: ' + enteredAddress);
+
+    //send this to google's API
+}
+
+form.addEventListener('submit', submitHandler);
